8c19d9bd3a11f65a18826d70fe4cb2263225f014
Élida <elidabeatriz13@gmail.com>
2022-05-10 03:41:58 -0300

    refactor: Adiciona métodos para listar usuários na page da Lista

--- cypress/support/pages/ListaPage.po.js
+++ cypress/support/pages/ListaPage.po.js
@@ -14,6 +14,31 @@ class ListaPage {
     verificarMensagemSucesso(mensagemSucesso){
         cy.contains(mensagemSucesso).should("be.visible");
     };
+
+    verificarMensagemErro(mensagemErro){
+        cy.contains(mensagemErro).should("be.visible");
+    };
+
+    verificarOpcao(opcao){
+        cy.contains(opcao).should("be.visible");
+    };
+
+    clicarEmProximaPagina(){
+        cy.get("#paginacaoProximo").click();
+    };
+
+    clicarEmVoltarPagina(){
+        cy.get("#paginacaoVoltar").click();
+    };
+
+    verificarListaUsuarios(){
+        cy.get("#listaUsuarios").should("be.visible");
+    };
+
+    verificarPaginacaoAtiva(paginacao){
+        cy.get(paginacao).should("not.be.disabled");
+    };
+    
 }
 
 export var listaPage = new ListaPage();
