64b13d6bc2d0ac5c661fd3e370232ebf5b7a4c91
Élida <elidabeatriz13@gmail.com>
2022-05-10 13:52:37 -0300

    refactor: Refatoração nos steps de Criar Usuário

--- cypress/support/steps/CriarUsuario.step.js
+++ cypress/support/steps/CriarUsuario.step.js
@@ -1,59 +1,64 @@
 import { cadastroPage } from "../pages/CadastroPage.po"
 
+Given("acessei a plataforma do CRUD FrontEnd", () => {
+    cadastroPage.visitar();
+});
+
 Given("acessei a tela de cadastro", () => {
+    cadastroPage.abrirFormularioCadastro();
+});
+
+When("informo os dados do usuário válido", (tabela) => {
     cy.intercept("https://crud-api-academy.herokuapp.com/api/v1/users", {
         fixture: "usuario.json",
         method: "POST", 
         statusCode: 201
     });
-    cadastroPage.visitar();
-    cadastroPage.abrirFormularioCadastro();
-});
-
-When("informo dados válidos", (tabela) => {
     var dadosTabela = tabela.rowsHash();
-    cadastroPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
+    cadastroPage.preencherFormulario(dadosTabela.nome, dadosTabela.email);
 });
 
-When("não informo dados", () => {
-    cadastroPage.clicarEmSalvar();
+When("não informo dados do usuário", () => {
+
 });
 
-When("informo email válido", (tabela) => {
+When("informo email de usuário válido", (tabela) => {
     var dadosTabela = tabela.rowsHash();
     cadastroPage.preencherEmail(dadosTabela.email);
-    cadastroPage.clicarEmSalvar();
 });
 
-When("informo nome válido", (tabela) => {
+When("informo nome de usuário válido", (tabela) => {
     var dadosTabela = tabela.rowsHash();
     cadastroPage.preencherNome(dadosTabela.nome);
-    cadastroPage.clicarEmSalvar();
 });
 
-When("informo email inválido", (tabela) => {
+When("informo email de usuário inválido", (tabela) => {
     var dadosTabela = tabela.rowsHash();
-    cadastroPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
+    cadastroPage.preencherFormulario(dadosTabela.nome, dadosTabela.email)
 });
 
-When("informo email já cadastrado", (tabela) => {
+When("informo email de usuário já cadastrado", (tabela) => {
     cy.intercept("https://crud-api-academy.herokuapp.com/api/v1/users", {
         fixture: "usuario.json",
         method: "POST", 
         statusCode: 422
     });
     var dadosTabela = tabela.rowsHash();
-    cadastroPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
+    cadastroPage.preencherFormulario(dadosTabela.nome, dadosTabela.email)
 });
 
 When("informo nome com mais de 100 caracteres", (tabela) => {
     var dadosTabela = tabela.rowsHash();
-    cadastroPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
+    cadastroPage.preencherFormulario(dadosTabela.nome, dadosTabela.email)
 });
 
 When("informo email com mais de 60 caracteres", (tabela) => {
     var dadosTabela = tabela.rowsHash();
-    cadastroPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
+    cadastroPage.preencherFormulario(dadosTabela.nome, dadosTabela.email)
+});
+
+When("salvo o formulário", () => {
+    cadastroPage.clicarEmSalvar();
 });
 
 Then("visualizo mensagem de sucesso {string}", (mensagemSucesso) => {
