07e6ae25194e0a3e411f7502bcf62b08be2f09e6
Élida <elidabeatriz13@gmail.com>
2022-05-10 12:41:33 -0300

    refactor: Adiciona intercept no step de Atualizar Usuário

--- cypress/support/steps/AtualizarUsuario.step.js
+++ cypress/support/steps/AtualizarUsuario.step.js
@@ -1,54 +1,63 @@
 import { detalhesPage } from "../pages/DetalhesPage.po"
 
 Given("acessei a tela de edição", () => {
+    cy.intercept("GET", "https://crud-api-academy.herokuapp.com/api/v1/users", {
+        statusCode: 200, 
+        fixture: "doisUsuarios.json"
+    });
     detalhesPage.visitar();
     detalhesPage.abrirFormularioEdicao();
 });
 
-When("informo dados válidos", (tabela) => {
+When("informo dados válidos para edição", (tabela) => {
     var dadosTabela = tabela.rowsHash();
     detalhesPage.limparFormulario()
     detalhesPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
 });
 
-When("não informo dados", () => {
+When("não informo dados para edição", () => {
     detalhesPage.limparFormulario();
     detalhesPage.clicarEmSalvar();
 });
 
-When("informo email válido", (tabela) => {
+When("informo email válido para edição", (tabela) => {
     var dadosTabela = tabela.rowsHash();
     detalhesPage.limparFormulario();
     detalhesPage.preencherEmail(dadosTabela.email);
     detalhesPage.clicarEmSalvar();
 });
 
-When("informo nome válido", (tabela) => {
+When("informo nome válido para edição", (tabela) => {
     var dadosTabela = tabela.rowsHash();
     detalhesPage.limparFormulario();
     detalhesPage.preencherNome(dadosTabela.nome);
     detalhesPage.clicarEmSalvar();
 });
 
-When("informo email inválido", (tabela) => {
+When("informo email inválido para edição", (tabela) => {
     var dadosTabela = tabela.rowsHash();
     detalhesPage.limparFormulario();
     detalhesPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
 });
 
-When("informo email já cadastrado", (tabela) => {
+When("informo email já cadastrado para edição", (tabela) => {
+    cy.intercept('PUT', "https://crud-api-academy.herokuapp.com/api/v1/users/33fe94c0-1765-46b5-aacf-fb78ece95805", {
+        statusCode: 422,
+        fixture: "usuario.json"
+        
+    });
     var dadosTabela = tabela.rowsHash();
     detalhesPage.limparFormulario();
     detalhesPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
 });
 
-When("informo nome com mais de 100 caracteres", (tabela) => {
+When("informo nome com mais de 100 caracteres para edição", (tabela) => {
     var dadosTabela = tabela.rowsHash();
     detalhesPage.limparFormulario();
     detalhesPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
 });
 
-When("informo email com mais de 60 caracteres", (tabela) => {
+When("informo email com mais de 60 caracteres para edição", (tabela) => {
     var dadosTabela = tabela.rowsHash();
     detalhesPage.limparFormulario();
     detalhesPage.preencherFormularioESalvar(dadosTabela.nome, dadosTabela.email)
