edf9b5ad79b8e451220aaf9a7ead2ef695aa73cf
Élida <elidabeatriz13@gmail.com>
2022-05-12 17:28:12 -0300

    refactor: Refatoração nos steps de Atualizar Usuário

--- cypress/support/steps/AtualizarUsuario.step.js
+++ cypress/support/steps/AtualizarUsuario.step.js
@@ -1,79 +1,82 @@
 import { detalhesPage } from "../pages/DetalhesPage.po"
 
-Given("acessei a plataforma do CRUD FrontEnd para edição", () => {
-    cy.intercept("GET", "https://crud-api-academy.herokuapp.com/api/v1/users", {
-        statusCode: 200, 
-        fixture: "doisUsuarios.json"
-    });
+const id = "11fe94c0-1765-46b5-aacf-fb78ece95801";
+
+Given("acessei a plataforma do CRUD FrontEnd", () => {
     detalhesPage.visitar();
 });
 
-Given("acessei a tela de edição", () => {
-    cy.intercept("GET", "https://crud-api-academy.herokuapp.com/api/v1/users/33fe94c0-1765-46b5-aacf-fb78ece95805", {
+And("existem usuários cadastrados", () => {
+    cy.intercept("GET", "https://crud-api-academy.herokuapp.com/api/v1/users", {
+        statusCode: 200,
+        fixture: "listaUsuarios.json"
+    });
+});
+
+Given("acessei a tela de detalhes de um usuário", () => {
+    cy.intercept("GET", `https://crud-api-academy.herokuapp.com/api/v1/users/${id}`, {
         statusCode: 200, 
         fixture: "usuario.json"
     });
-    detalhesPage.abrirFormularioEdicao();
+    detalhesPage.clicarEmVerDetalhes();
 });
 
-When("informo os dados do usuário válido para edição", (tabela) => {
-    cy.intercept('PUT', "https://crud-api-academy.herokuapp.com/api/v1/users/33fe94c0-1765-46b5-aacf-fb78ece95805", {
-        statusCode: 200,
-        fixture: "usuario.json"
-        
+When("informo dados do usuário para edição", (tabela) => {
+    cy.intercept('PUT', `https://crud-api-academy.herokuapp.com/api/v1/users/${id}`, {
+        statusCode: 200,        
     });
-    var dadosTabela = tabela.rowsHash();
-    detalhesPage.limparFormulario()
-    detalhesPage.preencherFormulario(dadosTabela.nome, dadosTabela.email)
+    const dadosTabela = tabela.rowsHash();
+    detalhesPage.limparFormulario();
+    detalhesPage.preencherFormulario(dadosTabela.nome, dadosTabela.email);
 });
 
 When("não informo dados do usuário para edição", () => {
     detalhesPage.limparFormulario();
 });
 
-When("informo email de usuário válido para edição", (tabela) => {
-    var dadosTabela = tabela.rowsHash();
+When("informo somente o email do usuário", (tabela) => {
+    const dadosTabela = tabela.rowsHash();
     detalhesPage.limparFormulario();
     detalhesPage.preencherEmail(dadosTabela.email);
 });
 
-When("informo nome de usuário válido para edição", (tabela) => {
-    var dadosTabela = tabela.rowsHash();
+When("informo somente o nome do usuário", (tabela) => {
+    const dadosTabela = tabela.rowsHash();
     detalhesPage.limparFormulario();
     detalhesPage.preencherNome(dadosTabela.nome);
 });
 
-When("informo email de usuário inválido para edição", (tabela) => {
-    var dadosTabela = tabela.rowsHash();
-    detalhesPage.limparFormulario();
-    detalhesPage.preencherFormulario(dadosTabela.nome, dadosTabela.email)
+When("informo email do usuário para edição", (tabela) => {
+    const dadosTabela = tabela.rowsHash();
+    detalhesPage.limparEmail();
+    detalhesPage.preencherEmail(dadosTabela.email);
 });
 
-When("informo email de usuário já cadastrado para edição", (tabela) => {
-    cy.intercept('PUT', "https://crud-api-academy.herokuapp.com/api/v1/users/33fe94c0-1765-46b5-aacf-fb78ece95805", {
-        statusCode: 422,
-        fixture: "usuario.json"
-        
+When("informo nome do usuário para edição", (tabela) => {
+    const dadosTabela = tabela.rowsHash();
+    detalhesPage.limparNome();
+    detalhesPage.preencherNome(dadosTabela.nome);
+});
+
+When("informo email já cadastrado do usuário para edição", (tabela) => {
+    cy.intercept('PUT', `https://crud-api-academy.herokuapp.com/api/v1/users/${id}`, {
+        statusCode: 422, 
     });
-    var dadosTabela = tabela.rowsHash();
-    detalhesPage.limparFormulario();
-    detalhesPage.preencherFormulario(dadosTabela.nome, dadosTabela.email)
-});
-
-When("informo nome com mais de 100 caracteres para edição", (tabela) => {
-    var dadosTabela = tabela.rowsHash();
-    detalhesPage.limparFormulario();
-    detalhesPage.preencherFormulario(dadosTabela.nome, dadosTabela.email)
-});
-
-When("informo email com mais de 60 caracteres para edição", (tabela) => {
-    var dadosTabela = tabela.rowsHash();
-    detalhesPage.limparFormulario();
-    detalhesPage.preencherFormulario(dadosTabela.nome, dadosTabela.email)
+    const dadosTabela = tabela.rowsHash();
+    detalhesPage.limparEmail();
+    detalhesPage.preencherEmail(dadosTabela.email);
 });
 
 When("salvo o formulário", () => {
     detalhesPage.clicarEmSalvar();
+});
+
+When("cancelo o formulário", () => {
+    detalhesPage.clicarEmCancelar();
+});
+
+When("verifico o campo id", () => {
+    detalhesPage.verificarCampoId(id);
 });
 
 Then("visualizo mensagem de sucesso {string}", (mensagemSucesso) => {
@@ -82,4 +85,16 @@ Then("visualizo mensagem de sucesso {string}", (mensagemSucesso) => {
 
 Then("visualizo mensagem de erro {string}", (mensagemErro) => {
     detalhesPage.verificarMensagemErro(mensagemErro);
+});
+
+Then("não deve ser possível editar os dados", () => {
+    detalhesPage.verificarCamposDesativados();
+});
+
+Then("visualizo a tela inicial", () => {
+    detalhesPage.verficiarAcessoPaginaInicial();
+});
+
+Then("o id ser igual ao informado na url", () => {
+    detalhesPage.verificarIdNaURL(id);
 });
