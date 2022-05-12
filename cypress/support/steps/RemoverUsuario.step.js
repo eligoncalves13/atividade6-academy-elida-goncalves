ea9e130e2bb5e5bf0a0149f25777060f5c78e631
Élida <elidabeatriz13@gmail.com>
2022-05-12 17:19:34 -0300

    refactor: Refatoração nos steps de Remover Usuário

--- cypress/support/steps/RemoverUsuario.step.js
+++ cypress/support/steps/RemoverUsuario.step.js
@@ -4,19 +4,37 @@ Given("acessei a plataforma do CRUD FrontEnd", () => {
     listaPage.visitar();
 });
 
-And("existem usuários cadastrados para remover", () => {
+And("existem usuários cadastrados", () => {
     cy.intercept("GET", "https://crud-api-academy.herokuapp.com/api/v1/users", {
         statusCode: 200,
-        fixture: "doisUsuarios.json"
+        fixture: "listaUsuarios.json"
     });
 });
 
 When("seleciono para remover um usuário", () => {
     listaPage.clicarEmRemover();
+});
+
+And("confirmo a remoção", () => {
     listaPage.clicarEmConfirmarRemover();
 });
 
-Then("visualizo mensagem de sucesso {string}", (mensagemSucesso) => {
-    cadastroPage.verificarMensagemSucesso(mensagemSucesso);
+And("cancelo a remoção", () => {
+    listaPage.clicarEmCancelarRemover();
 });
 
+And("seleciono o icone de fechar a modal", () => {
+    listaPage.clicarEmCancelarRemover();
+});
+
+Then("visualizo mensagem de sucesso {string}", (mensagemSucesso) => {
+    listaPage.verificarMensagemSucesso(mensagemSucesso);
+});
+
+Then("a modal é fechada", () => {
+    listaPage.verificarInexistenciaModal();
+});
+
+And("o usuário não é removido", () => {
+    listaPage.verificarUsuario("11fe94c0-1765-46b5-aacf-fb78ece95801");
+});
